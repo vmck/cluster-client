015fd58672ffa1eb7448410d1e0234a8852d2b6b
Alex Morega <alex@grep.ro>
2019-10-11 08:49:29 +0000

    Downgrade Nomad `v0.9.6` -> `v0.9.5`

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 (
   set -x
-  curl -OL https://releases.hashicorp.com/nomad/0.9.6/nomad_0.9.6_linux_amd64.zip
-  unzip nomad_0.9.6_linux_amd64.zip
+  curl -OL https://releases.hashicorp.com/nomad/0.9.5/nomad_0.9.5_linux_amd64.zip
+  unzip nomad_0.9.5_linux_amd64.zip
   sudo mv nomad /usr/local/bin/
-  rm nomad_0.9.6_linux_amd64.zip
+  rm nomad_0.9.5_linux_amd64.zip
 )
